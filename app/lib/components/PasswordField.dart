cd2345e8206d73c05056ff6d5f8e715c21f22655
gedeastu <gede.astu@gmail.com>
2023-12-24 04:12:30 +0700

    input

--- app/lib/components/PasswordField.dart
+++ app/lib/components/PasswordField.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class EmailField extends StatefulWidget {
+class PasswordField extends StatefulWidget {
   final controller;
   //final double width;
   final double rounded;
   //final double height;
   final TextInputType inputType;
   final String hintText;
-  const EmailField({
+  const PasswordField({
     super.key,
     required this.rounded,
     //required this.height,
@@ -18,10 +18,10 @@ class EmailField extends StatefulWidget {
   });
 
   @override
-  State<EmailField> createState() => _EmailFieldState();
+  State<PasswordField> createState() => _PasswordFieldState();
 }
 
-class _EmailFieldState extends State<EmailField> {
+class _PasswordFieldState extends State<PasswordField> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -29,7 +29,7 @@ class _EmailFieldState extends State<EmailField> {
       child: TextField(
           keyboardType: widget.inputType,
           controller: widget.controller,
-          obscureText: false,
+          obscureText: true,
           style: TextStyle(color: Colors.grey.shade200),
           decoration: InputDecoration(
             hintText: widget.hintText,
@@ -42,7 +42,7 @@ class _EmailFieldState extends State<EmailField> {
               borderRadius: BorderRadius.circular(widget.rounded)
             ),
             filled: true,
-            prefixIcon: Icon(Icons.email,color: Colors.blue,),
+            prefixIcon: Icon(Icons.lock,color: Colors.blue,),
             fillColor: Color.fromARGB(255, 43, 41, 55),
             enabledBorder:OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent)),
             focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.blue))
--- app/lib/pages/Login/mobile/LoginMobile.dart
+++ app/lib/pages/Login/mobile/LoginMobile.dart
@@ -1,4 +1,5 @@
 import 'package:app/components/EmailField.dart';
+import 'package:app/components/PasswordField.dart';
 import 'package:flutter/material.dart';
 
 class LoginMobile extends StatefulWidget {
@@ -18,29 +19,34 @@ class _LoginMobileState extends State<LoginMobile> {
         child: Column(
           children: [
             Container(
-              padding: EdgeInsets.all(25.0),
+              padding: EdgeInsets.only(left: 25.0,right: 25.0,top: 25.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text("Login",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),),
                   SizedBox(height: 5.0,),
-                  Text("Sign In to Continue",style: TextStyle(color: Colors.grey.shade600),)
-                ],
-              ),
-            ),
-            SizedBox(height: 40.0,),
-            Container(
-              child: Column(
-                children: [
+                  Text("Sign In to Continue",style: TextStyle(color: Colors.grey.shade600),),
+                  SizedBox(height: 50.0,),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("Email Address"),
-                      EmailField(controller: widget.emailController, width: 350, inputType: TextInputType.emailAddress, hintText: "Your Email Address")
+                      Text("Email Address",style: TextStyle(color: Colors.grey.shade200,fontWeight: FontWeight.normal,fontSize: 17),),
+                      SizedBox(height: 10.0,),
+                      EmailField(controller: widget.emailController, inputType: TextInputType.emailAddress, hintText: "Your Email Address",rounded: 80.0,)
+                    ],
+                  ),
+                  SizedBox(height: 25.0,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Password",style: TextStyle(color: Colors.grey.shade200,fontWeight: FontWeight.normal,fontSize: 17),),
+                      SizedBox(height: 10.0,),
+                      PasswordField(controller: widget.passwordController, inputType: TextInputType.visiblePassword, hintText: "Your Password",rounded: 80.0,)
                     ],
                   )
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
