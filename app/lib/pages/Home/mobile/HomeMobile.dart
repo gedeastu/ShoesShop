d2c82aab69da0f6af2c5c50c5e1a5d4e24036bfa
gedeastu <gede.astu@gmail.com>
2023-12-25 15:40:13 +0700

    up :  header home

--- app/lib/pages/Home/mobile/HomeMobile.dart
+++ app/lib/pages/Home/mobile/HomeMobile.dart
@@ -15,25 +15,60 @@ class _HomeMobileState extends State<HomeMobile> {
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 31, 29, 43),
         foregroundColor: Colors.grey.shade200,
-        leadingWidth: 200,
-        leading: Column(
-          //mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Hallo, Alex",style: GoogleFonts.poppins(
-              fontWeight: FontWeight.bold,
-              fontSize: 20
-            )),
-            Text("@alexkeinn")
-          ],
+        toolbarHeight: 80,
+        title: Container(
+          margin: EdgeInsets.only(left: 10),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Hallo, Alex",style: GoogleFonts.poppins(
+                fontWeight: FontWeight.bold,
+                fontSize: 20
+              )),
+              SizedBox(height: 3,),
+              Text("@alexkeinn",style: GoogleFonts.poppins(
+                color: Colors.grey.shade600,
+                fontSize: 15
+              ))
+            ],
+          ),
         ),
         actions: [
 
-          IconButton(onPressed: (){}, icon: Icon(Icons.account_circle_rounded,size: 40,),alignment: Alignment.center,)
+          Container(
+            height: 80,
+            width: 80,
+            margin: EdgeInsets.only(right: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(onPressed: (){}, icon: Icon(Icons.account_circle_rounded,size: 60,color: Colors.blueGrey.shade200,)),
+              ],
+            )
+          )
         ],
       ),
       body: SafeArea(
-        child: Text("Hello and Welcome")
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 25,vertical: 10),
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child:Row(
+                  children: [
+                    Text("Hello & Welcome",style: TextStyle(color: Colors.grey.shade200),),
+                    Text("Hello & Welcome",style: TextStyle(color: Colors.grey.shade200),),
+                    Text("Hello & Welcome",style: TextStyle(color: Colors.grey.shade200),),
+                    Text("Hello & Welcome",style: TextStyle(color: Colors.grey.shade200),)
+                  ],
+                ) ,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
