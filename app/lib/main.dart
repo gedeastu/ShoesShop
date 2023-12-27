835582b4ad6ace7107ea5f483fa0934a6de10740
gedeastu <gede.astu@gmail.com>
2023-12-28 00:23:19 +0700

    trying hive in terminal

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 //import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
-  await Hive.initFlutter();
+  //await Hive.initFlutter();
   //open the box
   //var box = await Hive.openBox('box');
   runApp(const Root());
