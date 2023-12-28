a83bbc40c861dea3c463e967cb1de88eaddfd9e5
gedeastu <gede.astu@gmail.com>
2023-12-29 01:42:07 +0700

    depedencies of firebase

--- shoesshop/lib/main.dart
+++ shoesshop/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:app/layouts/root.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 //import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   //await Hive.initFlutter();
   //open the box
   //var box = await Hive.openBox('box');
