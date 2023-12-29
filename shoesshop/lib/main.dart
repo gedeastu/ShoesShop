1670c859a12c86f20bf069051ceef195973bcdd7
gedeastu <gede.astu@gmail.com>
2023-12-29 14:51:14 +0700

    up: set gradle

--- shoesshop/lib/main.dart
+++ shoesshop/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 //import 'package:hive_flutter/hive_flutter.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   //await Hive.initFlutter();
